ece407d0e5e2fe69302ef4b5464b44c959dd735a
Daniel Almeida <64444838+danalmeidap@users.noreply.github.com>
2021-02-22 18:37:04 -0300

    Implements double-check validation

--- Jokenpo.py
+++ Jokenpo.py
@@ -4,28 +4,27 @@ from re import search, IGNORECASE
 
 def player_choice():
     """
-    Função em que o jogador escolhe entre as opções existentes para jogar ROck,Paper, Scissors
+    jogador escolhe entre as opções existentes.
     :return: A escolha do jogador entre as opções válidas.
     """
-    player = str(input('Choose Between Rock, Paper or Scissors')).strip().lower()
-    while player not in ['rock', 'paper', 'scissors']:
-        player = str(input('Invalid! Please choose between Rock, Paper or Scissors')).strip().lower()
+    player = str(input('Choose:[Rock, Paper or Scissors]: ')).strip().lower()
+    while player not in jaken_options:
+        player = str(input('Invalid!Rock, Paper or Scissors:')).strip().lower()
     return player
 
 
 def enemy_choice():
     """
-    Escolha do computador, baseado num random.choice entre as opções existentes.
+    Escolha do computador baseada nas opções existentes.
     :return: A escolha do adversário entre as opções válidas.
     """
-    jaken_options = ['rock', 'paper', 'scissors']
     enemy = choice(jaken_options)
     return enemy
 
 
 def jo_ken_po(player, computer):
     """
-    Baseado nas escolhas do jogador e do adversário, são feitas comparações e, assim obtemos o vencedor.
+    Realiza a jogada do jokenpo
     :param player: Escolha do jogador obtida pela função player_choice
     :param computer: Escolha do jogador obtida pela função player_choice
     :return: Função sem retorno
@@ -33,42 +32,61 @@ def jo_ken_po(player, computer):
     print(f'Player choice: {player}')
     print(f'Computer choice {computer}')
     print('-=-' * 15)
-    if not (not (player == 'rock' and computer == 'scissors') and not (
-            player == 'paper' and computer == 'rock')) or player == 'scissors' and computer == 'paper':
-        print(f'{player} beats {computer},You win!')
+    if (not (not ('rock' == player and computer == 'scissors')
+             and not (player == 'paper' and computer == 'rock')))\
+            or (not 'scissors' != player and computer == 'paper'):
+        winner = print_winner(player, computer, winner=True)
+        print(winner)
         print('Let s play again?')
+        print('-=-' * 15)
     elif player == computer:
-        print(f'We choose the same {player}. Tie.')
+        winner = print_winner(player, computer, tie=True)
+        print(winner)
         print('Let s play again?')
+        print('-=-' * 15)
     else:
-        print(f'{computer} beats {player}. You loose')
+        winner = print_winner(player, computer)
+        print(winner)
         print('Let s play again?')
         print('-=-' * 15)
 
 
 def joken_game():
     """
-    Parte de validações para o jogo, em que o jogador escolhe jogar ou não
+    Valida as entradas do usuário para jogar ou não.
     :return: função sem retorno
     """
     print('Do you want to play jo_ken_po?')
     while True:
-        answer = str(input('Answer:')).strip().lower()
+        answer = str(input('Answer: ')).strip().lower()
         if search("^y(es)?$", answer, IGNORECASE):
             player_1 = player_choice()
             player_2 = enemy_choice()
             jo_ken_po(player_1, player_2)
         elif search("^N(o)?$", answer, IGNORECASE):
-            break
+            answer = str(input('Are you sure? [Yes/No]: ')).strip().lower()
+            check = False
+            if search("^y(es)?$", answer, IGNORECASE):
+                print('Thank you for Playing')
+                print('-=-' * 15)
+                check = True
+            elif search("^N(o)?$", answer, IGNORECASE):
+                joken_game()
+            else:
+                print('Please, use only y(es) or n(o) for answer.')
+            if check:
+                break
         else:
             print('Please, use only y(es) or n(o) for answer.')
 
 
-try:
-    joken_game()
-except Exception as err:
-    print(f'Erro: {err}')
-else:
-    print('GAME OVER!')
-finally:
-    print('Thank you for playing')
+def print_winner(player1, enemy, winner=False, tie=False):
+    if winner:
+        return f'{player1} beats {enemy}, You win!'
+    if tie:
+        return 'We choose the same.Tie.'
+    return f'{enemy} beats {player1}, You loose!'
+
+
+jaken_options = ['rock', 'paper', 'scissors']
+joken_game()
