d3b5aaa5773d7a28be29a3395792cedb07360f98
danalmeidap <dbatistadealmeida23@gmail.com>
2021-03-01 14:46:02 -0300

    adjustments on dices_2 directory

--- dices_2/dices.py
+++ dices_2/dices.py
@@ -3,16 +3,21 @@ from re import search, IGNORECASE
 
 
 def value_check(msg):
+    """
+    Verify inputs value for only acept an integer
+    :param msg: text who will be show on the screen
+    :return: an intenger value
+    """
     while True:
         try:
-            n = int(input(msg))
+            value = int(input(msg))
         except(ValueError, TypeError):
             print('\033[0;31mPlease insert a valid integer.\033[m')
         except KeyboardInterrupt:
             print('\n\033[0;31mUser Interrupt;\033[m')
             return 0
         else:
-            return n
+            return value
 
 
 def rolling_dice(dices=1, minimum=1, maximum=2):
@@ -26,6 +31,19 @@ def rolling_dice(dices=1, minimum=1, maximum=2):
         cont += 1
 
 
+def confirm_no():
+    answer = str(input('Are you sure? [y/n]: ')).strip().lower()
+    is_yes = search("^y(es)?$", answer, IGNORECASE)
+    is_no = search("^n(o)?$", answer, IGNORECASE)
+    if is_yes or is_no:
+        if is_yes:
+            return True
+        else:
+            return False
+    else:
+        print('Please, use only y(es) or n(o) for answer.')
+
+
 class DiceSimulator:
     def __init__(self):
         self.minimum = 1
@@ -33,31 +51,32 @@ class DiceSimulator:
         self.message = 'Do you want to play dices? '
 
     def rolling(self):
+        check = False
         print(self.message, end='')
-        while True:
+        while not check:
             choice = str(input('Choice: ')).strip()
             print("-" * 15)
             # Checks for a valid input from user
-            if search("^y(es)?$", choice, IGNORECASE):
-                count = 0
-                while count != 1:
-                    dices = value_check("how many dices: ")
-                    self.maximum = value_check('What kind of dices: ')
-                    # checking input
-                    if self.maximum in dice_options and dices >= 1:
-                        rolling_dice(dices, self.minimum, self.maximum)  # Rolling the dice
-                        count += 1
-                    else:
-                        print('Please, check the values and try again')
-                print('Do you want to play again?')
-            # Exits when user says he will not play anymore
-            elif search("^n(o)?$", choice, IGNORECASE):
-                print('Logging out', flush=True)
-                sleep(0.5)
-                break
-            # Other possibilities
+            is_yes = search("^y(es)?$", choice, IGNORECASE)
+            is_no = search("^n(o)?$", choice, IGNORECASE)
+            if is_yes or is_no:
+                if is_yes:
+                    self.roll()
+                    print('Do you want to play again?')
+                else:
+                    check = confirm_no()
+
+    def roll(self):
+        count = 0
+        while count != 1:
+            dices = value_check("how many dices: ")
+            self.maximum = value_check('What kind of dices: ')
+            # checking input
+            if self.maximum in dice_options and dices >= 1:
+                rolling_dice(dices, self.minimum, self.maximum)  # Rolling the dice
+                count += 1
             else:
-                print('Please, use only y(es) or n(o) for answer.')
+                print('Please, check the values and try again')
 
 
 dice_options = (2, 4, 6, 8, 10, 12, 20, 100)
