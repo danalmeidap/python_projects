f5f726fe26b8a228ade6a8a191c9c9d3788f4396
danalmeidap <dbatistadealmeida23@gmail.com>
2021-03-01 14:30:34 -0300

    adjustments on jokenpo

--- Jokenpo.py
+++ Jokenpo.py
@@ -2,6 +2,12 @@ from random import choice
 from re import search, IGNORECASE
 
 
+def play_round():
+    player_1 = player_choice()
+    player_2 = computer_choice()
+    jo_ken_po(player_1, player_2)
+
+
 def player_choice():
     """
     jogador escolhe entre as opções existentes.
@@ -13,42 +19,27 @@ def player_choice():
     return player
 
 
-def enemy_choice():
+def computer_choice():
     """
     Escolha do computador baseada nas opções existentes.
     :return: A escolha do adversário entre as opções válidas.
     """
-    enemy = choice(jaken_options)
-    return enemy
+    cpu = choice(jaken_options)
+    return cpu
 
 
 def jo_ken_po(player, computer):
     """
     Realiza a jogada do jokenpo
     :param player: Escolha do jogador obtida pela função player_choice
-    :param computer: Escolha do jogador obtida pela função player_choice
+    :param computer: Escolha do jogador obtida pela função computer_choice
     :return: Função sem retorno
     """
     print(f'Player choice: {player}')
     print(f'Computer choice {computer}')
     print('-=-' * 15)
-    if (not (not ('rock' == player and computer == 'scissors')
-             and not (player == 'paper' and computer == 'rock')))\
-            or (not 'scissors' != player and computer == 'paper'):
-        winner = print_winner(player, computer, winner=True)
-        print(winner)
-        print('Let s play again?')
-        print('-=-' * 15)
-    elif player == computer:
-        winner = print_winner(player, computer, tie=True)
-        print(winner)
-        print('Let s play again?')
-        print('-=-' * 15)
-    else:
-        winner = print_winner(player, computer)
-        print(winner)
-        print('Let s play again?')
-        print('-=-' * 15)
+    result = check_play(player, computer)
+    print(result)
 
 
 def joken_game():
@@ -56,37 +47,48 @@ def joken_game():
     Valida as entradas do usuário para jogar ou não.
     :return: função sem retorno
     """
-    print('Do you want to play jo_ken_po?')
-    while True:
-        answer = str(input('Answer: ')).strip().lower()
-        if search("^y(es)?$", answer, IGNORECASE):
-            player_1 = player_choice()
-            player_2 = enemy_choice()
-            jo_ken_po(player_1, player_2)
-        elif search("^N(o)?$", answer, IGNORECASE):
-            answer = str(input('Are you sure? [Yes/No]: ')).strip().lower()
-            check = False
-            if search("^y(es)?$", answer, IGNORECASE):
-                print('Thank you for Playing')
-                print('-=-' * 15)
-                check = True
-            elif search("^N(o)?$", answer, IGNORECASE):
-                joken_game()
+
+    check = False
+    while not check:
+        answer = str(input('Do you wanna play a round of jo_ken_po? [y/n]: ')).strip().lower()
+        is_yes = search("^y(es)?$", answer, IGNORECASE)
+        is_no = search("^n(o)?$", answer, IGNORECASE)
+        if is_yes or is_no:
+            if is_yes:
+                play_round()
             else:
-                print('Please, use only y(es) or n(o) for answer.')
-            if check:
-                break
+                check = confirm_no()
         else:
             print('Please, use only y(es) or n(o) for answer.')
 
 
-def print_winner(player1, enemy, winner=False, tie=False):
-    if winner:
-        return f'{player1} beats {enemy}, You win!'
-    if tie:
-        return 'We choose the same.Tie.'
-    return f'{enemy} beats {player1}, You loose!'
+def confirm_no():
+    answer = str(input('Are you sure? [y/n]: ')).strip().lower()
+    is_yes = search("^y(es)?$", answer, IGNORECASE)
+    is_no = search("^n(o)?$", answer, IGNORECASE)
+    if is_yes or is_no:
+        if is_yes:
+            return True
+        else:
+            return False
+    else:
+        print('Please, use only y(es) or n(o) for answer.')
 
 
-jaken_options = ['rock', 'paper', 'scissors']
+def end_game():
+    print("*" * 15)
+    print('Thank you for playinh!')
+    print("*" * 15)
+
+
+def check_play(player1_choice, player_target):
+    if choice_map[player1_choice] == player_target:
+        return f'{player1_choice} beats {player_target}. You win!'
+    if choice_map[player_target] == player1_choice:
+        return f'{player_target} beats {player1_choice}. You loose!'
+    return 'We choose the same. Tie.'
+
+
+choice_map = {"rock": "scissors", "sicssors": "paper", "paper": "rock"}
+jaken_options = ["rock", "paper", "scissors"]
 joken_game()
